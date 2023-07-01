243b7da8a044935b46811603dc5ae8cb4181aeea
rokosius <bakunas.r@gmail.com>
2023-07-01 10:43:38 +0300

    update and optimize questios export module

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,36 +1,22 @@
 // unikalaus id generavimui
 const uniqid = require("uniqid");
-// importinam medeli 
+// importinam medeli
 const questionModel = require("./../models/question");
 //pass kodavimas
 const bcrypt = require("bcryptjs");
-//jwt token 
+//jwt token
 const jwt = require("jsonwebtoken");
 
+module.exports = {
+  addQuestion: async (req, res) => {
 
+  },
 
+  getQuestions: async (req, res) => {
 
+  },
 
-module.exports.addQuestion = async (req, res) => {
-
-}
-
-
-
-
-
-
-module.exports.getQuestions = async (req, res) => {
-
-}
-
-
-
-
-
-
-
-
-module.exports.getByIdQuestion = async (req, res) => {
-
-}
+  getByIdQuestion: async (req, res) => {
+    
+  },
+};
