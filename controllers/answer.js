49d9fa6f8dc880a7dd5512718b6c11679e9f04b8
rokosius <bakunas.r@gmail.com>
2023-07-01 12:42:06 +0300

    update

--- controllers/answer.js
+++ controllers/answer.js
@@ -1,24 +1,26 @@
 // unikalaus id generavimui
 const uniqid = require("uniqid");
-// importinam medeli 
+// importinam medeli
 const answerModel = require("./../models/answer");
 const questionModel = require("./../models/question");
 //pass kodavimas
 const bcrypt = require("bcryptjs");
-//jwt token 
+//jwt token
 const jwt = require("jsonwebtoken");
 
-
 module.exports = {
+  //pridedant ats į klausimą
+  addAnswerToQuestion: async (req, res) => {
+    //tikrinam ar atsakymas nera tuscias.
+    if (!req.body.answer_text || req.body.answer_text.trim() === "") {
+      return res
+        .status(400)
+        .json({
+          message: "Atsakymo negalima pridėti, nes jis nėra parašytas.",
+        });
+    }
 
-    //pridedant ats į klausimą
-    addAnswerToQuestion: async (req, res) => {
-        //tikrinam ar atsakymas nera tuscias.
-        if (!req.body.answer_text || req.body.answer_text.trim() === '') {
-            return res.status(400).json({ message: 'Atsakymo negalima pridėti, nes jis nėra parašytas.' });
-        }
-
-            //pridedam laukeli userId kuriame nurodoma klausimmo autorius (jo id)
+    //pridedam laukeli userId kuriame nurodoma klausimmo autorius (jo id)
     let userId;
     try {
       //gaunam jwt po to dekoduojam ir gaunam vartotojo id ir ji issaugome kartu su klausimu i userId
@@ -34,54 +36,56 @@ module.exports = {
         .json({ message: "Nepavyko patikrinti autentifikacijos." });
     }
 
+    const newAnswer = new answerModel({
+      answer_text: req.body.answer_text,
+      id: uniqid(),
+      question_id: req.body.question_id,
+      answerUserId: userId,
+    });
 
-    
-        const newAnswer = new answerModel({
-            answer_text: req.body.answer_text,
-            id: uniqid(),
-            question_id: req.body.question_id,
-            answerUserId: userId,
-        });
-    
-        try {
-            const question = await questionModel.findOne({ id: req.body.question_id });
-            if (!question) {
-                return res.status(404).json({ message: 'Klausimas nerastas.' });
-            }
-            const savedAnswer = await newAnswer.save();
-            question.answers_id.push(savedAnswer.id);
-            await question.save();
-            res.status(200).json({ message: 'Atsakymas sėkmingai pridėtas.' });
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    },
-
-
-
-// visi klausimo ats
-getAllQuestionAnswers: async (req, res) => {
     try {
-        const answers = await answerModel.find({ question_id: req.params.question_id });
-        res.status(200).json(answers);
+      const question = await questionModel.findOne({
+        id: req.body.question_id,
+      });
+      if (!question) {
+        console.log("err", question, req.body.question_id)
+
+        return res.status(404).json({ message: "Klausimas nerastas." });
+      }
+      const savedAnswer = await newAnswer.save();
+      question.answers_id.push(savedAnswer.id);
+      await question.save();
+      res.status(200).json({ message: "Atsakymas sėkmingai pridėtas." });
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json(err);
     }
-},
+  },
 
-
-// del atsakyma pagal id
-delAnswerById: async (req, res) => {
+  // visi klausimo ats
+  getAllQuestionAnswers: async (req, res) => {
     try {
-        const removedAnswer = await answerModel.deleteOne({ id: req.params.id });
-        if (removedAnswer.deletedCount > 0) {
-            res.status(200).json({ message: 'Atsakymas sėkmingai ištrintas. ' });
-        } else {
-            res.status(404).json({ message: 'Atsakymas nerastas.' });
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
-}
+      const answers = await answerModel.find({
+        question_id: req.params.question_id,
+      });
 
-}
+      if (answers.length === 0) {
+        res
+          .status(200).json({message: "Šis klausimas neturi jokių atsakymų. Buk pirmas ir atsakyk."});
+      } else { res.status(200).json(answers);}
+    } catch (err) {res.status(500).json(err);}
+  },
+
+  // del atsakyma pagal id
+  delAnswerById: async (req, res) => {
+    try {
+      const removedAnswer = await answerModel.deleteOne({ id: req.params.id });
+      if (removedAnswer.deletedCount > 0) {
+        res.status(200).json({ message: "Atsakymas sėkmingai ištrintas. " });
+      } else {
+        res.status(404).json({ message: "Atsakymas nerastas." });
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
